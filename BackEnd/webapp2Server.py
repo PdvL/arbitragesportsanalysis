eff8e4dd92ef384392abd54dceac7911eaa0fb3d
wacax <wacax@hotmail.com>
2014-06-14 17:07:33 -0500

    various fixes

--- BackEnd/webapp2Server.py
+++ BackEnd/webapp2Server.py
@@ -12,7 +12,7 @@ form = """
 class MainPage(webapp2.RequestHandler):
 
     def get(self):
-#        self.response.headers['Content-Type'] = 'text/plain'
+##        self.response.headers['Content-Type'] = 'text/plain'
         self.response.write(form)
 
 
